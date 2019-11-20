56eb293336a9d6cc167c9fbddf407c59209cdb2a
Herve Desrosiers <herve_desrosiers@hotmail.com>
2019-11-20 11:11:05 -0500

    downloaded DesignCode v4 for scroll parallax start

---
+++ DesignCodeApp 4/DesignCodeApp/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  DesignCodeApp
+//
+//  Created by Meng To on 11/14/17.
+//  Copyright © 2017 Meng To. All rights reserved.
+//
+
+import UIKit
+import AVKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var deviceImageView: UIImageView!
+    @IBOutlet weak var playVisualEffectView: UIVisualEffectView!
+    
+    @IBAction func playButtonTapped(_ sender: Any) {
+        let urlString = "https://player.vimeo.com/external/235468301.hd.mp4?s=e852004d6a46ce569fcf6ef02a7d291ea581358e&profile_id=175"
+        let url = URL(string: urlString)
+        let player = AVPlayer(url: url!)
+        let playerController = AVPlayerViewController()
+        playerController.player = player
+        present(playerController, animated: true) {
+            player.play()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        titleLabel.alpha = 0
+        deviceImageView.alpha = 0
+        playVisualEffectView.alpha = 0
+        
+        UIView.animate(withDuration: 1) {
+            self.titleLabel.alpha = 1
+            self.deviceImageView.alpha = 1
+            self.playVisualEffectView.alpha = 1
+        }
+    }
+}
